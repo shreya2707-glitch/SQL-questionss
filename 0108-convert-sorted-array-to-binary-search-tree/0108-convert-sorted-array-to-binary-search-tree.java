b52dbd992bf36d52ebd97bc7f49980f4f16c2669
Shreya Panda <shreyapanda2707@gmail.com>
2026-09-19 13:30:13 +0530

    Time: 0 ms (100%), Space: 44.8 MB (80.65%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
@@ -0,0 +1,21 @@
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return buildBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildBST(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        // Choose the middle element as root to ensure height balance
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+
+        // Recursively construct left and right subtrees
+        root.left = buildBST(nums, left, mid - 1);
+        root.right = buildBST(nums, mid + 1, right);
+
+        return root;
+    }
+}
