f3eb4472de33ce933f250e0d662b8b3a4f1d668b
Shreya Panda <shreyapanda2707@gmail.com>
2026-09-25 12:57:53 +0530

    Time: 0 ms (100%), Space: 42.7 MB (11.74%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        
+        while (n != 0) {
+            n &= (n - 1); // Clears the lowest set bit
+            count++;
+        }
+        
+        return count;
+    }
+}
