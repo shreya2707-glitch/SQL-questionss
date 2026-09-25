b730d07b620ca1c07cf654c30edfbaee3da9edfc
Shreya Panda <shreyapanda2707@gmail.com>
2026-09-25 13:48:35 +0530

    Time: 2 ms (100%), Space: 46.2 MB (92.98%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int romanToInt(String s) {
+        int total = 0;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            int currentVal = getValue(s.charAt(i));
+
+            // If the next numeral exists and has a larger value, subtract currentVal
+            if (i + 1 < n && currentVal < getValue(s.charAt(i + 1))) {
+                total -= currentVal;
+            } else {
+                total += currentVal;
+            }
+        }
+
+        return total;
+    }
+
+    private int getValue(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+}
